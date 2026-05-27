caa459effc0ab005e497d08a0772e7618b4bf476
Madhavkrshah <madhavkrshah@gmail.com>
2026-05-27 21:32:18 +0530

    Initialize a todo app with Context API and localStorage

---
+++ todoAppwithcAPInLS/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='text-center bg-purple-500 p-4'>Todo App</h1></>
+  )
+}
+
+export default App

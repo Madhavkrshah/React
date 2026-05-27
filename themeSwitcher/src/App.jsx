3b4be3ca228a537b83d4daf4a36bb7b4275c8946
Madhavkrshah <madhavkrshah@gmail.com>
2026-05-27 17:15:49 +0530

    Add theme toggle button and card component

--- themeSwitcher/src/App.jsx
+++ themeSwitcher/src/App.jsx
@@ -1,12 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { ThemeProvider } from "./Context/theme";
+import ThemeBtn from "./Components/ThemeBtn";
+import Card from "./Components/Card";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  // Actual chang in theme
+  const HTML = document.querySelector("html");
+  useEffect(() => {
+    HTML.classList.remove("light", "dark");
+    HTML.classList.add(themeMode);
+  }, [themeMode]);
 
   return (
-    <>
-      <h1 className="text-center bg-yellow-300 p-4">Theme Switcher App</h1>
-    </>
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 

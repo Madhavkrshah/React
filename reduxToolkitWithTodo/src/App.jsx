fd23ec783e7ea7202241c6fe2894d7f18f2d2faa
Madhavkrshah <madhavkrshah@gmail.com>
2026-06-02 23:53:53 +0530

    App layout done - AddTodo and Todos sitting together, looks good

--- reduxToolkitWithTodo/src/App.jsx
+++ reduxToolkitWithTodo/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -6,6 +8,9 @@ function App() {
   return (
     <>
       <h1>Learn about Redux ToolKit</h1>
+      <AddTodo />
+      <Todos />
+      {/* </div> */}
     </>
   );
 }

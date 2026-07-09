ea1123109956ccf501d575020d2756be06c989b2
Madhavkrshah <madhavkrshah@gmail.com>
2026-07-09 23:19:52 +0530

    Import all code files of components folder and export them from index file

--- MegaBlogApp/src/components/index.js
+++ MegaBlogApp/src/components/index.js
@@ -3,5 +3,29 @@ import Header from "./Header/Header";
 import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./PostForm/PostForm";
+import Button from "./Button";
+import Input from "./Input";
+import Select from "./Select";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
 
-export { Footer, Header, Container, Logo, LogoutBtn };
+export {
+  Footer,
+  Header,
+  Container,
+  Logo,
+  LogoutBtn,
+  RTE,
+  Signup,
+  Login,
+  PostForm,
+  Button,
+  Input,
+  Select,
+  PostCard,
+  AuthLayout,
+};
